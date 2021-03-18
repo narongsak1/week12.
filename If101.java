901b97417b35a7c5a36c0fe0cddf90e5597957bf
narongsak1 <69183697+narongsak1@users.noreply.github.com>
2021-03-18 13:04:59 +0700

    Add files via upload

---
+++ If101.java
@@ -0,0 +1,14 @@
+package week12;
+
+public class If101 {
+	
+	public static void main(String[]args) {
+		int num = 10;
+		if(num % 5 == 0)
+		{
+			System.out.println("We are inside the if-block");
+		}
+		System.out.println("We are outside the if-block");
+	}
+
+}
